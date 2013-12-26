959a0b40c3ce69c93980d9ed845d5e076a28f7e3
Daniel Soto <dsoto@drs-mba.local>
2013-12-26 09:18:16 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# PyPVSim
+
+A library for solar PV energy calculations focusing on availability of
+power.
---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name='PyPVSim',
+    packages=['pypvsim',],
+    version='0.1dev',
+)
